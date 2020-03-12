857a00691c810607992e8f920ef8e2d2982b9890
Corrigan Newsome <cnewsome2@cnm.edu>
2020-03-12 13:44:23 -0600

    added a few basic headings for the website.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>my-pwp-corrigan</title>
+</head>
+<body>
+<h1>Corrigan's Personal Webpage</h1>
+<div>
+	<h2>Purpose</h2>
+	<p></p>
+	<h2>Audience</h2>
+	<p></p>
+	<h2>Goal</h2>
+	<p></p>
+	<h2>Persona</h2>
+	<p></p>
+</div>
+</body>
+</html>

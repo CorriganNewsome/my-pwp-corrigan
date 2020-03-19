977d6daca389e2a29ea9108aef3d4eff8ebbbc1d
Corrigan Newsome <cnewsome2@cnm.edu>
2020-03-19 10:50:42 -0600

    added lawyer-photo.jpg, and added content under all four headers.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,16 +5,39 @@
 	<title>my-pwp-corrigan</title>
 </head>
 <body>
+
 <h1>Corrigan's Personal Webpage</h1>
+
 <div>
-	<h2>Purpose</h2>
-	<p></p>
-	<h2>Audience</h2>
-	<p></p>
-	<h2>Goal</h2>
-	<p></p>
+	<img src="lawyer-photo.jpg" alt = "lawyer-photo" id = "lawyer-photo">
+
 	<h2>Persona</h2>
-	<p></p>
+		<li>Name: Oliver Manchester Esq.</li>
+		<li>Age: 49</li>
+		<li>Occupation: Lawyer</li>
+		<li>Technology used: Dell laptop, Windows OS, Apple iPhone. </li>
+		<li>Goals: To be able to inquire about services, prices, and timelines for depositions quickly and easily,
+		to be able to book an appointment on the website, to be able to leave contact information in a "contact us form"
+		on the website.</li>
+		<li>Frustrations: Not being able to get in contact with Court Reporters and not being able to find one on short notice.</li>
+		<ul>Oliver is a 49 year old lawyer that uses a variety of different technologies. Oliver is fairly technically sound, however
+		the website doesn't need to be incredibly difficult to navigate. A few basic links at the top and information displayed in every
+		link needs to be found quickly and easily. Oliver wants to be able to go into the website, book an appointment and leave the website
+		in the shortest amount of time possible.</ul>
+
+	<h2>Purpose</h2>
+		<p>The purpose of this website is to help my younger brother with his Court Reporting Business and helping him be
+	more visible to potential clients.</p>
+
+	<h2>Audience</h2>
+		<p>Lawyers who need to inquire about Court Reporting services, prices, and timelines for depositions.</p>
+
+	<h2>Goals</h2>
+		<p>The goals of this website is to:
+		<li>Bring more traffic to my younger brothers business</li>
+		<li>Which will increase revenue and to make him more visible in the community.</li>
+	</p>
+
 </div>
 </body>
 </html>

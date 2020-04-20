3ffcf1b4a7614cc91c9de4ccfed2272990771d42
Corrigan Newsome <cnewsome2@cnm.edu>
2020-04-19 19:38:32 -0600

    added form

--- mileston-3.php
+++ mileston-3.php
@@ -11,6 +11,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 </head>
 <body>
+<div>
 
 <nav class="navbar navbar-expand-lg navbar-light">
 	<div class="container">
@@ -20,11 +21,49 @@
 	</div>
 </nav>
 
+<div class="container">
 <div class="jumbotron jumbotron-fluid bg-light">
 	<div class="container">
 		<h1 class="display-4">Doncaster Court Reporting LLC.</h1>
 	</div>
 </div>
+</div>
+
+
+
+	<div class="container">
+	<form>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Name</label>
+			<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="First Last">
+		</div>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Phone Number</label>
+			<input type="tel" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+		</div>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Business Address</label>
+			<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="123 Main St">
+		</div>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Email address</label>
+			<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="555-555-5555">
+		</div>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Date of Deposition</label>
+			<input type="date" class="form-control" id="exampleFormControlInput1" placeholder="01/01/2000">
+		</div>
+		<div class="form-group">
+			<label for="exampleFormControlTextarea1">Message</label>
+			<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+		</div>
+	</form>
+	</div>
+
+
+
+
+
 
 
 </body>
